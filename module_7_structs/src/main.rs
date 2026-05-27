662b1a36dcb5fffc6cba2de5e4f1c2c2511c49b4
Kinan Dak Albab <kinan.bab.14@gmail.com>
2026-05-27 17:45:09 -0400

    Structs and enums

--- module_7_structs/src/main.rs
+++ module_7_structs/src/main.rs
@@ -1,28 +1,24 @@
-struct BankAccount {
-    pub balance: i32,
-    pub transactions: Vec<i32>,
-}
+mod types;
+
+use types::BankAccount;
+use types::Transaction;
 
 fn main() {
-    let mut account = BankAccount {
-        balance: 0,
-        transactions: Vec::new()
-    };
+    let mut account = BankAccount::new();
 
     // I deposited 100$
-    account.balance += 100;
-    account.transactions.push(100);
+    account.make_transaction(Transaction::Deposit(100));
 
     // I withdrew 20$
-    account.balance -= 20;
-    account.transactions.push(-20);
+    account.make_transaction(Transaction::Withdrawal(-20));
 
     // I withdrew another 30$
-    account.balance -= 30;
-    account.transactions.push(-30);
+    account.make_transaction(Transaction::Withdrawal(-30));
 
     // What's my current balance?
-    println!("{}", account.balance);
-    
-    // How many withdrawals have I done overall?
+    account.print_balance();
+    account.count_withdrawals();
+
+    let new_account = BankAccount::new();
+    new_account.count_withdrawals();
 }
