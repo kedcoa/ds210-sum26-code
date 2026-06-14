a66afd2db177772f483d15712352c3eb76e3cc57
kedcoa <emilyren@bu.edu>
2026-06-14 01:06:56 -0400

    added filter solution

--- project_4_client_server_analytics/analytics_lib/src/solution.rs
+++ project_4_client_server_analytics/analytics_lib/src/solution.rs
@@ -2,8 +2,35 @@ use std::collections::HashMap;
 use crate::dataset::{ColumnType, Dataset, Value, Row};
 use crate::query::{Aggregation, Condition, Query};
 
+//create helper function to determine if one or more conditions happen
+
+pub fn row_matches (row: &Row, condition: &Condition, dataset: &Dataset) -> bool {
+    match condition {
+        //1. equal condition
+        Condition::Equal(col_name, expected) => {
+            let index = dataset.column_index (col_name);
+            row.get_value(index) == expected
+        }
+        Condition::Not(inner) => {
+            !row_matches(row, inner, dataset)
+        }
+        Condition::And(left, right) => {
+            row_matches(row, left, dataset) && row_matches(row, right, dataset)
+        }
+        Condition::Or(left, right)=> {
+            row_matches(row, left, dataset) || row_matches (row, right, dataset)
+        }
+    }
+}
+
 pub fn filter_dataset(dataset: &Dataset, filter: &Condition) -> Dataset {
-    todo!("Implement this!");
+    let mut result = Dataset::new(dataset.columns().clone());
+    for row in dataset.iter() {
+        if row_matches(row, filter, dataset) {
+            result.add_row(row.clone());
+        }
+    }
+    result
 }
 
 pub fn group_by_dataset(dataset: Dataset, group_by_column: &String) -> HashMap<Value, Dataset> {
